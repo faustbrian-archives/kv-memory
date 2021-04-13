f62ad40861245d3b60c10a4a2bc4a7b161b62374
Brian Faust <hello@basecode.sh>
2021-04-13 16:45:34 +0300

    chore: integrate `@konceiver/tease`

---
+++ src/async.test.ts
@@ -0,0 +1,11 @@
+import { complianceTestsAsync } from "@konceiver/kv-test-suite";
+
+import { StoreAsync } from "../src/async";
+
+complianceTestsAsync(() => StoreAsync.new<string, number>(), {
+	1: 1,
+	2: 2,
+	3: 3,
+	4: 4,
+	5: 5,
+});
